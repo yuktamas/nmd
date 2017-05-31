231700c712db7f1080c20102329d5bc458d65228
munnadroid <gotameraju2@gmail.com>
2017-05-31 12:48:13 +0545

    adding doctor detail fragment

---
+++ app/src/main/java/com/awecode/nmd/view/specialist/doctor/DoctorDetailFragment.kt
@@ -0,0 +1,11 @@
+package com.awecode.nmd.view.specialist.doctor
+
+import com.awecode.nmd.R
+import com.awecode.stockapp.view.base.BaseFragment
+
+/**
+ * Created by munnadroid on 5/31/17.
+ */
+class DoctorDetailFragment:BaseFragment(){
+    override val layoutId: Int= R.layout.fragment_doctor_detail
+}
